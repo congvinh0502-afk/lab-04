38a74e54fa4e2e119a5aeafacdda1fd5edbf1402
Vinh <congvinh0502@gmail.com>
2026-05-15 16:55:54 +0700

    Update Store to support Media

--- src/hust/soict/dsai/aims/store/Store.java
+++ src/hust/soict/dsai/aims/store/Store.java
@@ -1,22 +1,57 @@
 package hust.soict.dsai.aims.store;
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
-public class Store {
-    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[100];
-    private int qty = 0;
 
-    public void addDVD(DigitalVideoDisc dvd) {
-        itemsInStore[qty++] = dvd;
+import hust.soict.dsai.aims.media.Media;
+import java.util.ArrayList;
+
+public class Store {
+
+    private ArrayList<Media> itemsInStore =
+            new ArrayList<Media>();
+
+    // Add media
+    public void addMedia(Media media) {
+
+        if (!itemsInStore.contains(media)) {
+
+            itemsInStore.add(media);
+
+            System.out.println("The media has been added to store");
+
+        } else {
+
+            System.out.println("The media already exists in store");
+        }
     }
 
-    public void removeDVD(DigitalVideoDisc dvd) {
-        for (int i = 0; i < qty; i++) {
-            if (itemsInStore[i] == dvd) {
-                for (int j = i; j < qty - 1; j++) {
-                    itemsInStore[j] = itemsInStore[j + 1];
-                }
-                qty--;
-                break;
-            }
+    // Remove media
+    public void removeMedia(Media media) {
+
+        if (itemsInStore.contains(media)) {
+
+            itemsInStore.remove(media);
+
+            System.out.println("The media has been removed from store");
+
+        } else {
+
+            System.out.println("The media is not found in store");
         }
+    }
+
+    // Print store
+    public void printStore() {
+
+        System.out.println("****************STORE****************");
+
+        for (Media media : itemsInStore) {
+            System.out.println(media.toString());
+        }
+
+        System.out.println("*************************************");
+    }
+
+    // Getter
+    public ArrayList<Media> getItemsInStore() {
+        return itemsInStore;
     }
 }
